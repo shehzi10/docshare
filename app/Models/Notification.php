ac15631439cf93b93533f50a7463682e16d909a3
Shehzi <shehzi.moin10@gmail.com>
2022-06-15 20:13:41 +0500

    First Api's Shehzi Push

---
+++ app/Models/Notification.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Notification extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['sender_id', 'reciever_id', 'type', 'content_id', 'title', 'body'];
+
+
+    public function from(){
+        return $this->belongsTo(User::class, 'sender_id', 'id');
+    }
+
+    public function to(){
+        return $this->belongsTo(User::class, 'reciever_id', 'id');
+    }
+}
---
+++ app/Models/UserFriend.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserFriend extends Model
+{
+    use HasFactory;
+
+
+    protected $fillable = ['user_id', 'requested_user_id', 'is_followed', 'followed_back', 'status', 'accepted_date'];
+
+
+
+    public function requestedUser(){
+        return $this->belongsTo(User::class, 'requested_user_id', 'id');
+    }
+}
